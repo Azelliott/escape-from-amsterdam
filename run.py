fb6bb610ddeb2dd1f5569d59db7cd68e922d3371
Azelliott <jukic.alen@gmail.com>
2022-04-12 19:33:50 +0000

    Add intro

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ import os
 import time
 
 
+
 def clr_scr():
     # Check if operating system is Linux or Mac
     if os.name == 'posix':
@@ -47,4 +48,39 @@ def splash_end():
                       |_|                      
     ''')
 
-splash_start()
+def intro():
+    splash_start()
+    name = str(input("Enter your name:\n> "))
+    clr_scr()
+    print("You slowly open your eyes...\n")
+    time.sleep(2)
+    print("what the hell did I do last night...")
+    print("my head is killing me.")
+    time.sleep(1)
+    print("Oh yeah... I'm in Amsterdam\n")
+    time.sleep(2)
+    print("You look around, room is trashed like there was a rockstar party inside.")
+    print("There are bottles, clothes and half finished drinks everywere\n")
+    time.sleep(2)
+    print("Where is everybody?\n")
+    time.sleep(2)
+    print("On a night stand next to you there is a message\n")
+    time.sleep(4)
+    print("It's written in cursive on a thick paper,")
+    print("you can smell the scent of women's parfume on it.")
+    print("There is a kiss in the corner made of red lipstick.\n")
+    print("It reads:\n")
+    time.sleep(4)
+    print("Dear " + name)
+    print("welcome to AMS Liberia.")
+    print("We hope you had a good night,")
+    print("now, get ready for the first game.")
+    print("Time is of essence and you have excatly 60 minutes")
+    print("to find the combination and open the door.")
+    print("If you fail, door will stay locked...Forever.")
+    print("Good luck.")
+    print("            Miss V")
+    time.sleep(3)
+    print("\nYou get up and after looking around identify several interesting items\n")
+
+intro()
